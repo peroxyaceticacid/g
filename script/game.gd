42b98c0e7b4f3ca0825ac5bc05f8b1afebf55ba5
acid <aiden.l.jones10@gmail.com>
2025-08-07 14:11:38 -0700

    particle added pls add functionality

--- script/game.gd
+++ script/game.gd
@@ -1,12 +1,27 @@
 extends Node2D
 
-# objects
-@onready var GClick = $obj_GButton
+# ANIMATIONS
+@onready var ButtonAnimationIDLEPlayer = $Animations/anim_GButtonIdle
+@onready var ButtonAnimationCLICKPlayer = $Animations/anim_GButtonClick
+
+# PARTICLES
+@onready var G_EXPLODE = $Particles/particle_GExplode
+
+# PRELOAD SCENES
+var particlescene = preload("res://scenes/particles.tscn")
 
 func _ready() -> void:
+	ButtonAnimationIDLEPlayer.play("anim_GBIdle")
 	pass
 
 func _onclick():
+	if ButtonAnimationCLICKPlayer.is_playing() == true:
+		ButtonAnimationCLICKPlayer.stop()
+	ButtonAnimationCLICKPlayer.play("anim_GBClick")
+	
+	var explosioneffect = particlescene.instantiate()
+	
+	
 	print("test")
 
 func _process(delta: float) -> void:
